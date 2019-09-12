d657262e42a4118ddca62fa4805abf3152876b42
m_cor <mikecorbridge@gmail.com>
2019-09-12 13:44:20 -0400

    format the matrix to remove unused item add more comments

--- testingRecursion.py
+++ testingRecursion.py
@@ -8,40 +8,41 @@
 
 class TestingRecursion:
 
+    # set up the sample 2x2 grid (square)
     def __init__(self):
 
-        self.row0 = [['o', '', 0, (0, 0)], ['o', '', 0, (0, 1)], ['o', '', 0, (0, 2)], ['o', '', 0, (0, 3)],
-                     ['o', '', 0, (0, 4)]]
-        self.row1 = [['o', '', 0, (1, 0)], ['x', '', 0, (1, 1)], ['x', '', 0, (1, 2)], ['o', '', 0, (1, 3)],
-                     ['o', '', 0, (1, 4)]]
-        self.row2 = [['o', '', 0, (2, 0)], ['x', '', 0, (2, 1)], ['x', '', 0, (2, 2)], ['o', '', 0, (2, 3)],
-                     ['o', '', 0, (2, 4)]]
-        self.row3 = [['o', '', 0, (3, 0)], ['x', '', 0, (3, 1)], ['o', '', 0, (3, 2)], ['o', '', 0, (3, 3)],
-                     ['o', '', 0, (3, 4)]]
-        self.row4 = [['o', '', 0, (4, 0)], ['o', '', 0, (4, 1)], ['o', '', 0, (4, 2)], ['o', '', 0, (4, 3)],
-                     ['o', '', 0, (4, 4)]]
+        self.row0 = [['o', 0, (0, 0)], ['o', 0, (0, 1)], ['o', 0, (0, 2)], ['o', 0, (0, 3)],
+                     ['o', 0, (0, 4)]]
+        self.row1 = [['o', 0, (1, 0)], ['x', 0, (1, 1)], ['x', 0, (1, 2)], ['o', 0, (1, 3)],
+                     ['o', 0, (1, 4)]]
+        self.row2 = [['o', 0, (2, 0)], ['x', 0, (2, 1)], ['x', 0, (2, 2)], ['o', 0, (2, 3)],
+                     ['o', 0, (2, 4)]]
+        self.row3 = [['o', 0, (3, 0)], ['x', 0, (3, 1)], ['o', 0, (3, 2)], ['o', 0, (3, 3)],
+                     ['o', 0, (3, 4)]]
+        self.row4 = [['o', 0, (4, 0)], ['o', 0, (4, 1)], ['o', 0, (4, 2)], ['o', 0, (4, 3)],
+                     ['o', 0, (4, 4)]]
 
-        self.row5 = [['o', '', 0, (0, 0)], ['o', '', 0, (0, 1)], ['o', '', 0, (0, 2)], ['o', '', 0, (0, 3)],
-                     ['o', '', 0, (0, 4)]]
-        self.row6 = [['o', '', 0, (1, 0)], ['x', '', 0, (1, 1)], ['o', '', 0, (1, 2)], ['x', '', 0, (1, 3)],
-                     ['o', '', 0, (1, 4)]]
-        self.row7 = [['o', '', 0, (2, 0)], ['x', '', 0, (2, 1)], ['o', '', 0, (2, 2)], ['x', '', 0, (2, 3)],
-                     ['o', '', 0, (2, 4)]]
-        self.row8 = [['o', '', 0, (3, 0)], ['x', '', 0, (3, 1)], ['x', '', 0, (3, 2)], ['x', '', 0, (3, 3)],
-                     ['o', '', 0, (3, 4)]]
-        self.row9 = [['o', '', 0, (4, 0)], ['o', '', 0, (4, 1)], ['o', '', 0, (4, 2)], ['o', '', 0, (4, 3)],
-                     ['o', '', 0, (4, 4)]]
+        self.row5 = [['o', 0, (0, 0)], ['o', 0, (0, 1)], ['o', 0, (0, 2)], ['o', 0, (0, 3)],
+                     ['o', 0, (0, 4)]]
+        self.row6 = [['o', 0, (1, 0)], ['x', 0, (1, 1)], ['o', 0, (1, 2)], ['x', 0, (1, 3)],
+                     ['o', 0, (1, 4)]]
+        self.row7 = [['o', 0, (2, 0)], ['x', 0, (2, 1)], ['o', 0, (2, 2)], ['x', 0, (2, 3)],
+                     ['o', 0, (2, 4)]]
+        self.row8 = [['o', 0, (3, 0)], ['x', 0, (3, 1)], ['x', 0, (3, 2)], ['x', 0, (3, 3)],
+                     ['o', 0, (3, 4)]]
+        self.row9 = [['o', 0, (4, 0)], ['o', 0, (4, 1)], ['o', 0, (4, 2)], ['o', 0, (4, 3)],
+                     ['o', 0, (4, 4)]]
 
-        self.row10 = [['x', '', 0, (0, 0)], ['x', '', 0, (0, 1)], ['o', '', 0, (0, 2)], ['o', '', 0, (0, 3)],
-                     ['o', '', 0, (0, 4)]]
-        self.row11 = [['o', '', 0, (1, 0)], ['o', '', 0, (1, 1)], ['o', '', 0, (1, 2)], ['o', '', 0, (1, 3)],
-                     ['o', '', 0, (1, 4)]]
-        self.row12 = [['o', '', 0, (2, 0)], ['o', '', 0, (2, 1)], ['o', '', 0, (2, 2)], ['o', '', 0, (2, 3)],
-                     ['o', '', 0, (2, 4)]]
-        self.row13 = [['o', '', 0, (3, 0)], ['o', '', 0, (3, 1)], ['o', '', 0, (3, 2)], ['o', '', 0, (3, 3)],
-                     ['o', '', 0, (3, 4)]]
-        self.row14 = [['o', '', 0, (4, 0)], ['o', '', 0, (4, 1)], ['o', '', 0, (4, 2)], ['o', '', 0, (4, 3)],
-                     ['x', '', 0, (4, 4)]]
+        self.row10 = [['x', 0, (0, 0)], ['x', 0, (0, 1)], ['o', 0, (0, 2)], ['o', 0, (0, 3)],
+                     ['o', 0, (0, 4)]]
+        self.row11 = [['o', 0, (1, 0)], ['o', 0, (1, 1)], ['o', 0, (1, 2)], ['o', 0, (1, 3)],
+                     ['o', 0, (1, 4)]]
+        self.row12 = [['o', 0, (2, 0)], ['o', 0, (2, 1)], ['x', 0, (2, 2)], ['o', 0, (2, 3)],
+                     ['o', 0, (2, 4)]]
+        self.row13 = [['o', 0, (3, 0)], ['o', 0, (3, 1)], ['o', 0, (3, 2)], ['o', 0, (3, 3)],
+                     ['o', 0, (3, 4)]]
+        self.row14 = [['o', 0, (4, 0)], ['o', 0, (4, 1)], ['o', 0, (4, 2)], ['o', 0, (4, 3)],
+                     ['x', 0, (4, 4)]]
 
         # self.square = [self.row0, self.row1, self.row2, self.row3, self.row4]
         # self.square = [self.row5, self.row6, self.row7, self.row8, self.row9]
@@ -49,43 +50,49 @@ class TestingRecursion:
 
         self.islandNum = 0
 
+    # print out the square with annotations showing island associations
     def printSquare(self):
         for row in self.square:
             print(str(row))
 
+    # this starts the process to find the islands by viewing each row in the square
     def iterateSquare(self):
         for row in self.square:
             self.iterateRow(row)
 
+    # inspect each cell in the row to determine if it is part of an island ('x')
     def iterateRow(self, row):
         for cell in row:
-            if cell[0] == 'x' and cell[2] == 0:
-                self.islandNum = self.islandNum + 1
-                cell[2] = self.islandNum
+            if cell[0] == 'x' and cell[1] == 0:
+                self.islandNum = self.islandNum + 1 # <---- here is (only) where the island number is incremented
+                cell[1] = self.islandNum
                 self.xNeighbor(cell)
 
+    # inspect the neighbour cell, recursively, directly to the right (x-axis) & below (y-axis) to determine if those
+    # cells have an 'x' property that indicates they should be grouped together as an island
     def xNeighbor(self, cell):
-        if cell[3][0] != 4 and cell[3][1] != 4:
-            numCellRbeside = cell[3][1] + 1
-            numRowBeneath = cell[3][0] + 1
-            cellRbeside = self.square[cell[3][1]][numCellRbeside]
-            cellBelow = self.square[numRowBeneath][cell[3][1]]
+        if cell[2][0] != 4 and cell[2][1] != 4:
+            numCellRbeside = cell[2][1] + 1
+            numRowBeneath = cell[2][0] + 1
+            cellRbeside = self.square[cell[2][1]][numCellRbeside]
+            cellBelow = self.square[numRowBeneath][cell[2][1]]
             adjacentCells = []
 
-            if cellRbeside[0] == 'x' and cellRbeside[2] == 0:
+            # the cell beside the current cell is an 'x' and is currently not part of an island
+            if cellRbeside[0] == 'x' and cellRbeside[1] == 0:
                 adjacentCells.append(cellRbeside)
-                cellRbeside[2] = cell[2]
+                cellRbeside[1] = cell[1]
 
-            if cellBelow[0] == 'x' and cellBelow[2] == 0:
+            # the cell beneath the current cell is an 'x' and is currently not part of an island
+            if cellBelow[0] == 'x' and cellBelow[1] == 0:
                 adjacentCells.append(cellBelow)
-                cellBelow[2] = cell[2]
-
+                cellBelow[1] = cell[1]
+            # inspect these cells recursively (if they exist) to determine their relationship
             if len(adjacentCells) != 0:
                 for aCell in adjacentCells:
-                    self.xNeighbor(aCell)
-            else:
-                print("no more cells adjacent to: " + str(cell))
+                    self.xNeighbor(aCell)  # <----- recursion!
 
+    # print the number of islands found
     def getNumIslands(self):
         print("number of islands = " + str(self.islandNum))
 
