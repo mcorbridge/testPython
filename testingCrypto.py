a673c76bc2fccacf1c2ea4a0c7f507fe12da2bd7
m_cor <mikecorbridge@gmail.com>
2019-09-13 20:12:52 -0400

    add original author

--- testingCrypto.py
+++ testingCrypto.py
@@ -4,7 +4,7 @@ from hashlib import md5
 from base64 import b64encode, b64decode
 
 
-# from --> https://gist.github.com/Frizz925/ac0fb026314807959db5685ac149ed67
+# from --> https://gist.github.com/Frizz925/ac0fb026314807959db5685ac149ed67  Izra Faturrahman
 
 class TestingCrypto:
 
