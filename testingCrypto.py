67efd067fb9ba122acba6bdc65cf24505843a4de
m_cor <mikecorbridge@gmail.com>
2019-09-13 20:05:13 -0400

    a new passphrase

--- testingCrypto.py
+++ testingCrypto.py
@@ -3,6 +3,7 @@ from Crypto.Util import Padding
 from hashlib import md5
 from base64 import b64encode, b64decode
 
+
 # from --> https://gist.github.com/Frizz925/ac0fb026314807959db5685ac149ed67
 
 class TestingCrypto:
@@ -10,7 +11,7 @@ class TestingCrypto:
     def __init__(self):
         print("Testing Crypto")
 
-        self.passphrase = "foo"
+        self.passphrase = "Wally"
         self.content = ""
         self.mode = AES.MODE_CBC
         self.bs = AES.block_size
@@ -30,7 +31,7 @@ class TestingCrypto:
         print("encrypted text:")
         print(self.result)
 
-    def decrypt(self,encryptedData, passphrase):
+    def decrypt(self, encryptedData, passphrase):
         print("decrypt :")
         try:
             key, body, iv = encryptedData.split(".")
@@ -44,19 +45,21 @@ class TestingCrypto:
         except:
             print("WRONG PASSWORD! Nice try asshole!")
 
-plaintext = "Beto O'Rourke's best moment on Thursday's Democratic presidential debate -- which also doubled as his best"\
-"moment in the 2020 campaign to date -- came when ABC's David Muir asked whether he supported a mandatory buyback of"\
-"assault weapons. Hell, yes, we're going to take your AR-15, your AK-47, O'Rourke said to raucous applause from the"\
-"crowd in Houston, Texas. We're not going to allow it to be used against our fellow Americans anymore."
+
+plaintext = "Beto O'Rourke's best moment on Thursday's Democratic presidential debate -- which also doubled as his best" \
+"moment in the 2020 campaign to date -- came when ABC's David Muir asked whether he supported a mandatory " \
+"buyback of assault weapons. Hell, yes, we're going to take your AR-15, your AK-47, O'Rourke said to raucous " \
+"applause from the crowd in Houston, Texas. We're not going to allow it to be used against our fellow"\
+"Americans anymore."
 
 testingCrypto = TestingCrypto()
 testingCrypto.encrypt(plaintext)
 
-encryptedData = "YWNiZDE4ZGI0Y2MyZjg1Y2VkZWY2NTRmY2NjNGE0ZDg=.xMcPOvog0HsV+4CxbmD2tjSGn+AhnQ6JJVN7rbDtEHUEmfP8ROFcYPY3"\
-"okA7zaMsoLXKOU2RbfwQRzWzIBuVZ4i39/f6X8MmE9cP7EeJ1Aq6rZdLCdwQkErR2a4hPHv7r+VEdTOdOlS2vP+dYddK/Vj+qW7SYb3mDAtnP8fnK"\
-"jw5M/Wx6au78coT0thEJI6xPE91tOT2NXsF9D4eWs5g1it8gX5adcIy4XG2zIvnXC65QlQIWdGQ/ueDsPH9/4vJsugkqKhr1ZFykz3IKi63i9v8+Bb8zwr1+md"\
-"FvE06ZmEDLFxZ0yiSub2lvutAicIMPB2ul78y+CqETbVQ0sN8+jNfYxOz+LHrms+5gGw6DPWDihT3sD+tYidENTVNyFzeZVe6gyICAqxsfcDOlsIzWXkVmvgly"\
-"IdguFZLdd+dtJxdyqHkNlpauvV1lUQfuoPBWTk+dNHv413TPwCf3MIdrrCmAR7rGOaMkRTamn/4IM7vLLPNXU413vuAFw9RqRUMb87EnO9K0"\
-"gow11mNy29JSNuA1Tj9oPiCwzn0Fv/fzh8NARX4fZh8xd2qqRhmN26qSQyRxlTsBm1IT80pbjk4Yw==.NGNjMmY4NWNlZGVmNjU0Zg=="
+encryptedData = "ODc4OWI0NWRlMzI4MDFhZDIyMGY1OWU2NmQyOWYzZTk=.hf3dzkSVEXPTMysfox+maP7S7Qj/dTfOcP2bKOGPnQGw/L6JaK+0nMTAs"\
+"R9dfZGcUUtL/D+mgAnB+Q8ZKRFcCUorCmyhbo3BT5q8Wkt6d+pgqQYuzz0ECx9jAiI3K/VvlD3zYlOc1G0WHbVP1JtJqSu5ZwB7tibkdm578oDbEIx/fBMq"\
+"2enoe36l+WmbGvBkNr8lMCwj3M1snezCE4hrPQPRC8sU/Ld1ZbxlJ+r29r18y2m2JO37lcrUKAu3zH5oIrTDTvm1OK4UX2qhMyzu/2Quj2tZ/6vwLpZzW5M"\
+"WTDLBUU69r+E1L8EUAWdEtqb/4aM3CdKT5Ls6O7WePww68N4ZXUxRUun3QJ6RBMqTVyeKvS2cfXGI18ce8pDNGbcVTTnqOZUjh7JhjDqfFF62Phc8zjCmoV"\
+"j2/vYiVVOEeMdMWUSeOh0TclFiCsNREZJ/EMDyWn8v4ZdHoiRxlgqsgj0gQQuCzTUKCaHy73uWWdluZ7s+U3lZ8qXkiBPdkSZZAikMprChQypfSE2dI1Hfl"\
+"vRrJaqAwMf316FYklDaWEUQi5sfPvCEwpdtYEs4/hfzwgoZLQ9rxdErMjUCN548Ag==.ZTMyODAxYWQyMjBmNTllNg=="
 
-testingCrypto.decrypt(encryptedData,"foo")
+testingCrypto.decrypt(encryptedData, "Wally")
