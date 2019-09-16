58ce233958678618ee0551bc0d1305bb54cdd026
m_cor <mikecorbridge@gmail.com>
2019-09-16 14:59:49 -0400

    POST data to a test python server

---
+++ testRequestResponse.py
@@ -0,0 +1,25 @@
+import requests # https://2.python-requests.org/en/master/#
+
+
+class TestRequestResponse:
+
+    def __init__(self):
+        print("TestRequestResponse init")
+
+    def doRequest(self):
+        payload = {'key1': 'value1'}
+        requests.post('http://127.0.0.1:8080/testPost', data=payload)
+
+    def doResponse(self):
+        r = requests.get('http://127.0.0.1:8080/')
+        print(r.text)
+
+
+
+testRequestResponse = TestRequestResponse()
+isTrue = False
+if isTrue:
+    testRequestResponse.doRequest()
+else:
+    testRequestResponse.doResponse()
+
